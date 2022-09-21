cb0c9954c7c6863f68c4d1480e6f0eb8f3d0653d
Francisco Rodriguez <franciscorodriguezsv24@gmail.com>
2022-09-20 19:36:38 -0600

    se actualizo el code

---
+++ Practicas/Hoisting/Ejercicio-de-hoisting.js
@@ -0,0 +1,7 @@
+function hello(){
+    console.log("Hello" + miNombre);
+}
+
+var miNombre = "Fermando"
+
+//Hoisting es una manera de referirse cuando se declara despues de haberse inicializado una variable o function. lo mas recomendable siempre es declarar variables y functions antes de inicializarse para no tener ni un solo error en el  codigo. 
